141d7d1e2ed77511c8579cb9436e7d7d25b41ffe
Komaromi-Balint-HPBPCL <155990917+KomY28@users.noreply.github.com>
2025-05-09 08:01:31 +0200

    Add files via upload

---
+++ Telefonszamok_DAL_Konzol/Telefonszamok_DAL_Konzol/Bovito.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Telefonszamok_DAL_Konzol;
+namespace Telefonszamok_WPF
+{
+    public static class Bovito
+    {
+        public static string Telefonszamok(this enSzemely enSzemely)
+        {
+            var s = "";
+            foreach (var x in enSzemely.enTelefonszam)
+            {
+                s = s + x.Szam;
+                if (x != enSzemely.enTelefonszam.Last())
+                    s = s + ", ";
+            }
+            return s;
+        }
+    }
+}
---
+++ Telefonszamok_DAL_Konzol/Telefonszamok_DAL_Konzol/enSzemelyKieg.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace Telefonszamok_DAL_Konzol
+{
+    public partial class enSzemely
+    {
+        public string Telefonszamok
+        {
+            get
+            {
+                var s = "";
+                foreach (var x in enTelefonszam)
+                {
+                    s = s + x.Szam;
+                    if (x != enTelefonszam.Last())
+                        s = s + ", ";
+                }
+                return s;
+            }
+        }
+    }
+}
